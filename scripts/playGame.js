e10d059811fcfb1d0a8d988e6d05d5d7495b3bf4
Sultana247 <jahansultana302@gmail.com>
2025-11-06 11:41:41 +0600

    finished play buttons work with javascript

--- scripts/playGame.js
+++ scripts/playGame.js
@@ -1,4 +1,9 @@
+
+
 function playPage(){
      hideElementByID('home-page');
      displayElementByID('play-game');
+     const ranAlpha = randomAlpha();
+     setElementById('key', ranAlpha);
+     changeColorbyId(ranAlpha);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -6,3 +6,22 @@ function displayElementByID(Id){
     const element = document.getElementById(Id);
     element.classList.remove('hidden');
 }
+
+function randomAlpha(){
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    const randomNum = Math.random()*25;
+    const index = Math.round(randomNum);
+    const randomAlphabet = alphabets[index];
+    return randomAlphabet;
+    
+}
+function setElementById(Id, final){
+    const element =document.getElementById(Id);
+    element.innerText = final;
+}
+
+function changeColorbyId(Id){
+    const key = document.getElementById(Id);
+    key.classList.add('bg-orange-400');
+}
